2471e57742d9ec9e7ac2d63c7236e8f31340f56a
Ayleen Jimenez <ayleen.jimenez01@outlook.com>
2025-03-06 18:31:35 -0600

    add past files

--- classProblems/2.2.py
+++ classProblems/2.2.py
@@ -1,39 +1,41 @@
-def best_set(votes):
-    maxVotes = {}
-    for artist in votes.values():
-        if artist in maxVotes:
-            maxVotes[artist] += 1
-        else:
-            maxVotes[artist] = 1
+# def count_endangered_species(endangered_species, observed_species):
+#     e_set = set(endangered_species)
 
-    # maxNum = -1
-    # maxNames = ""
-    # for name in maxVotes.keys():
-    #     if maxVotes[name] > maxNum:
-    #         maxNum = maxVotes[name]
-    #         maxNames = name
+#     count = 0
+#     for species in observed_species: #might change to o_set
+#         if species in e_set:
+#             count += 1
+#     return count
 
-    # maxV = maxVotes.get(max(maxVotes.values()))
+# endangered_species1 = "aA"
+# observed_species1 = "aAAbbbb"
 
-    return max(maxVotes, key = maxVotes.get)
+# endangered_species2 = "z"
+# observed_species2 = "ZZ"
+
+# print(count_endangered_species(endangered_species1, observed_species1)) 
+# print(count_endangered_species(endangered_species2, observed_species2))  
 
 
-votes1 = {
-    1234: "SZA", 
-    1235: "Yo-Yo Ma",
-    1236: "Ethel Cain",
-    1237: "Ethel Cain",
-    1238: "SZA",
-    1239: "SZA"
-}
 
-votes2 = {
-    1234: "SZA", 
-    1235: "Yo-Yo Ma",
-    1236: "Ethel Cain",
-    1237: "Ethel Cain",
-    1238: "SZA"
-}
+def navigate_research_station(station_layout, observations):
+    letter_to_index = {char: index for index, char in enumerate(station_layout)}
+    curr_pos = letter_to_index[observations[0]] 
+    total_time = curr_pos
 
-print(best_set(votes1))
-print(best_set(votes2))
+    #print(f'{letter_to_index}')
+    for observation in observations[1:]:
+        next_pos = letter_to_index[observation]  
+        total_time += abs(curr_pos - next_pos)  
+        curr_pos = next_pos 
+    
+    return total_time
+
+station_layout1 = "pqrstuvwxyzabcdefghijklmno"
+observations1 = "wildlife"
+
+station_layout2 = "abcdefghijklmnopqrstuvwxyz"
+observations2 = "cba"
+
+print(navigate_research_station(station_layout1, observations1))  
+print(navigate_research_station(station_layout2, observations2))
